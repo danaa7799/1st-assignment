4e99bf000b0cbb23c8f670b54ab3947f30b70bc7
Dana Atias <danaa7799@gmail.com>
2022-10-22 16:03:24 +0300

    adding a Django develipment environment
    - Setup [Pipenv][1] to install and manage Django and other Python
    dependencies
    - Bootstrapped an empty [Django][2] application
    
    After running  the application it can be accessed at:
    > http://127.0.0.1:8000/
    
    **Note:** Most changes had been auto-generated, only `Vagrantfile` and
    `steup.sh` had been edited manually.
    
    [1]: https://github.com/pypa/pipenv
    [2]: https://www.djangoproject.com/

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -ex
+brew install pipenv
+# install pipenv
+
+cd pipenv
+#enter to pipenv folder
+
+pipenv sync --dev
+#install dependencies with pipenv
+
+pipenv run python manage.py runserver 0.0.0.0:8000 > runserver.log
+#run the skeleton application
